661f5238d4b53255dedcc559bfa09dc055f0e5b8
Marco Fratini <marcofratini@outlook.com>
2023-05-16 17:56:56 +0200

    Remove second parameter from characterList function

--- src/main.ts
+++ src/main.ts
@@ -11,15 +11,15 @@ const characterList: any = createCharacterList();
 
 const html = document.querySelector<HTMLDivElement>('.characters-list')!;
 
-const joffrey = kingTemplate(characterList[0], 'img/joffrey.jpg');
+const joffrey = kingTemplate(characterList[0]);
 
-const jamie = swordsmanTemplate(characterList[1], 'img/jaime.jpg');
+const jamie = swordsmanTemplate(characterList[1]);
 
-const daenerys = swordsmanTemplate(characterList[2], 'img/daenerys.jpg');
+const daenerys = swordsmanTemplate(characterList[2]);
 
-const tyrion = kingshandTemplate(characterList[3], 'img/tyrion.jpg');
+const tyrion = kingshandTemplate(characterList[3]);
 
-const broon = squireTemplate(characterList[4], 'img/bronn.jpg');
+const broon = squireTemplate(characterList[4]);
 
 html.innerHTML = joffrey + jamie + daenerys + tyrion + broon;
 
